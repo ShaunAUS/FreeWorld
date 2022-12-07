68661f0f6b165e296909d5e69b2be2e36839665c
minjae <86937655+ShaunAUS@users.noreply.github.com>
2022-12-07 15:22:29 +0900

    free-1 : skeleton API

---
+++ mjV1/src/main/java/com/example/testSecurity/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package com.example.testSecurity.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleController {
+
+    //TODO 등록
+    //TODO 수정
+    //TODO 삭제
+    //TODO 디테일
+    //TODO 북마크
+}
