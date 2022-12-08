bd9a747f8ad31937adde442af6706001f1a18669
minjae <86937655+ShaunAUS@users.noreply.github.com>
2022-12-08 11:30:37 +0900

    free-3 redis: error fix

--- mjV1/src/main/java/com/example/testSecurity/TestSecurityApplication.java
+++ mjV1/src/main/java/com/example/testSecurity/TestSecurityApplication.java
@@ -2,10 +2,12 @@ package com.example.testSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = "com.example.testSecurity.repository")
 public class TestSecurityApplication {
 
 	public static void main(String[] args) {
