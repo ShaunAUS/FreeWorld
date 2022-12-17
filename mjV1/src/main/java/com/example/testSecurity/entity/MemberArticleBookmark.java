0a23fc94c13efc0e7d2d898b9c7707ba576b6a1e
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-12-17 21:49:29 +0900

    free-4 : fix usless gralde options, andd MemberArticleBookmark entity

---
+++ mjV1/src/main/java/com/example/testSecurity/entity/MemberArticleBookmark.java
@@ -0,0 +1,29 @@
+package com.example.testSecurity.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberArticleBookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_article_bookmark_no")
+    private Long no;
+
+    @ApiModelProperty(value = "멤버 no")
+    @OneToOne
+    @JoinColumn(name = "member_no")
+    private Member member;
+    @ApiModelProperty(value = "게시글 no")
+    @OneToOne
+    @JoinColumn(name = "article_no")
+    private Article article;
+
+}
