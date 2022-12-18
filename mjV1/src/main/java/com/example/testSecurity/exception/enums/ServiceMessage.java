0ec3d9acef056ab271c5fd46d7e4c60b34df4b32
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-12-18 20:58:51 +0900

    free-6 : add Excpetion message

--- mjV1/src/main/java/com/example/testSecurity/exception/enums/ServiceMessage.java
+++ mjV1/src/main/java/com/example/testSecurity/exception/enums/ServiceMessage.java
@@ -14,7 +14,8 @@ public enum ServiceMessage {
     NOT_FOUND_ACCESS_INFO(-3,"접근기록을 찾을 수 없습니다" ),
 
     NOT_AUTHORIZED(-4,"인증되지 않은 사용자" ),
-    NOT_FOUND(-5,"해당하는 정보가 없습니다." );
+    NOT_FOUND(-5,"해당하는 정보가 없습니다." ),
+    COMPANY_NOT_FOUND(-6, "해당 회사가 존재하지 않습니다" );
 
 
     
