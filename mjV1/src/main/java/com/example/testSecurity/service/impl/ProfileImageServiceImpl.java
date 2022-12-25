2b6bccce86415e1dea0fd82b1793fa64097ee1c5
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-12-25 12:07:54 +0900

    free-9: ProfileImageService/ Impl /entity/controller

---
+++ mjV1/src/main/java/com/example/testSecurity/service/impl/ProfileImageServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.testSecurity.service.impl;
+
+import com.example.testSecurity.dto.ImageInfo;
+import com.example.testSecurity.entity.Profile;
+import com.example.testSecurity.exception.ServiceProcessException;
+import com.example.testSecurity.exception.enums.ServiceMessage;
+import com.example.testSecurity.querydlsRepository.ProfileImageCustomRepository;
+import com.example.testSecurity.repository.ProfileJpaRepository;
+import com.example.testSecurity.service.ImageService;
+import com.example.testSecurity.service.ProfileImageService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+public class ProfileImageServiceImpl implements ProfileImageService {
+
+    private final ImageService imageService;
+    private final ProfileImageCustomRepository profileImageCustomRepository;
+    private final ProfileJpaRepository profileJpaRepository;
+
+
+    @Override
+    public List<ImageInfo> registerImage(List<MultipartFile> imageList, Long profileNo) {
+
+        Profile profileById = profileJpaRepository.findById(profileNo).get();
+
+        //사진 저장
+        List<ImageInfo> imageInfoList = imageService.uploadImage(imageList, profileNo);
+
+        //URL 저장
+        for (ImageInfo imageInfo : imageInfoList) {
+
+            if (imageInfo == null || imageInfo.getUrl() == null) {
+                throw new ServiceProcessException(ServiceMessage.IMAGE_LOAD_ERROR);
+            }
+
+            profileImageCustomRepository.insert(imageInfo.getUrl(), profileById,
+                countImages(profileNo) + 1);
+        }
+        return imageInfoList;
+    }
+
+
+    @Override
+    public int countImages(Long profileNo) {
+        return profileImageCustomRepository.countCurrentImages(profileNo);
+    }
+}
