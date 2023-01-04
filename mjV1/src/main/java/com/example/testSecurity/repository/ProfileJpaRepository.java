73951165a1d214342d1c58a927e56956210a17be
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2023-01-04 17:34:31 +0900

    free-11: fix CategoryType Enum and ProfileRepository

--- mjV1/src/main/java/com/example/testSecurity/repository/ProfileJpaRepository.java
+++ mjV1/src/main/java/com/example/testSecurity/repository/ProfileJpaRepository.java
@@ -1,9 +1,11 @@
 package com.example.testSecurity.repository;
 
 import com.example.testSecurity.entity.Profile;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface ProfileJpaRepository extends JpaRepository<Profile, Long> {
 
+    Optional<Profile> findByMemberNo(Long loginMemberNo);
 }
