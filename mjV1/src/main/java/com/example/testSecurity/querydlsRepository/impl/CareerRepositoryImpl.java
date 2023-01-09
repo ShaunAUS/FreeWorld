84897d12abebcfc9e3ff39df7550caa7f9b593c5
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2023-01-09 16:16:02 +0900

    free-12: fix CareerRepo

---
+++ mjV1/src/main/java/com/example/testSecurity/querydlsRepository/impl/CareerRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.example.testSecurity.querydlsRepository.impl;
+
+import static com.example.testSecurity.entity.QCareer.career;
+
+import com.example.testSecurity.dto.CareerDto;
+import com.example.testSecurity.dto.CareerDto.Create;
+import com.example.testSecurity.entity.Career;
+import com.example.testSecurity.querydlsRepository.CareerCustomRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CareerRepositoryImpl implements CareerCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public CareerRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    //career -> career.create
+    @Override
+    public List<Career> findByProfileNo(Long profileNo) {
+
+        return queryFactory
+            .select(career)
+            .from(career)
+            .where(career.profile.no.eq(profileNo))
+            .fetch();
+
+    }
+}
--- mjV1/src/main/java/com/example/testSecurity/repository/CareerJpaRepository.java
+++ mjV1/src/main/java/com/example/testSecurity/repository/CareerJpaRepository.java
@@ -1,9 +1,14 @@
 package com.example.testSecurity.repository;
 
 import com.example.testSecurity.entity.Career;
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface CareerJpaRepository extends JpaRepository<Career, Long> {
 
+    List<Career> findByProfileNo(Long profileNo);
+
+    
 }
