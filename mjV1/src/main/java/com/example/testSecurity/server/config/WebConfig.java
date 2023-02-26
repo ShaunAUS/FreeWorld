914f464502f7881c2c4117b24d77a1243ed2610f
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2023-02-26 17:42:18 +0900

    free-31 : WebConfig 정리 , 중복 정리

--- mjV1/src/main/java/com/example/testSecurity/server/config/WebConfig.java
+++ mjV1/src/main/java/com/example/testSecurity/server/config/WebConfig.java
@@ -1,11 +1,14 @@
-package com.example.testSecurity.service.config;
+package com.example.testSecurity.server.config;
 
 import com.example.testSecurity.service.aop.LoginMemberArgumentResolver;
 import com.example.testSecurity.service.service.MemberService;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,9 +17,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final MemberService memberService;
 
+    //AOP @LoginMember
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberService));
+    }
+
+    //MvcConfiguration
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/templates/")
+            .setCacheControl(CacheControl.maxAge(15, TimeUnit.SECONDS));
     }
 }
 
